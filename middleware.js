d711464dd6619ae76a0dce25b09f0246439aa8b0
Mateus Mota <mateuslourencomota@hotmail.com>
2024-05-28 22:24:41 -0300

    add middleware

--- middleware.js
+++ middleware.js
@@ -1,15 +1,15 @@
 import { NextResponse } from 'next/server';
 // import { supabase } from './lib/supabaseClient';
 import { updateSession } from './utils/supabase/middleware';
+import { createClient } from './utils/supabase/server';
+import { getUser } from './app/login/api/login';
 
 export async function middleware(req) {
   const res = NextResponse.next();
-  // const data = async () => {
-  //   return await supabase.auth.getUser()
-  // };
-  // if (!data) {
-  //   return NextResponse.redirect(new URL('/login', req.url));
-  // }
+  const data = await getUser()
+   if (data.data.user == null) {
+    return NextResponse.redirect(new URL('/login', req.url));
+  }
 
   return await updateSession(req);
 }
